82fe7547be6f9b3dea5a2701c4e2735bbcb552ba
Warren Buckley <warren@creativewebspecialist.co.uk>
2014-12-06 22:30:05 +0000

    AuthToken Factory & Secret Key from Environment Variable

---
+++ Umbraco.AuthTokens/Data/UmbracoAuthTokenFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using JWT;
+
+namespace Umbraco.AuthTokens.Data
+{
+    public static class UmbracoAuthTokenFactory
+    {
+        static string _secretKey = Environment.GetEnvironmentVariable("Umbraco.AuthToken", EnvironmentVariableTarget.Machine);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="authToken"></param>
+        /// <returns></returns>
+        public static UmbracoAuthToken GenerateUserAuthToken(UmbracoAuthToken authToken)
+        {
+            //Date Time
+            var dateCreated = DateTime.UtcNow;
+            var dateCreatedToString = dateCreated.ToString("u");
+
+            //Create JSON payload for JWT token
+            var payload = new Dictionary<string, object>() {
+                { "user_id", authToken.UserId },
+                { "username", authToken.UserName },
+                { "user_type", authToken.UserType },
+                { "date_created", dateCreatedToString }
+            };
+
+            //Encode the JWT token with JSON payload, algorithm & our secret in constant
+            var encodedToken = JsonWebToken.Encode(payload, _secretKey, JwtHashAlgorithm.HS256);
+
+            //Return same object we passed in (Now with Date Created & Token properties updated)
+            authToken.DateCreated = dateCreated;
+            authToken.AuthToken = encodedToken;
+
+            //Return the updated object
+            return authToken;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jwtToken"></param>
+        /// <returns></returns>
+        public static UmbracoAuthToken DecodeUserAuthToken(string jwtToken)
+        {
+            //Object to return
+            var userAuth = new UmbracoAuthToken();
+
+            //Decode & verify token was signed with our secret
+            var jsonPayload = JsonWebToken.DecodeToObject(jwtToken, _secretKey) as IDictionary<string, object>;
+
+            //Just the presence of the token & being deserialised with correct SECRET key is a good sign
+            if (jsonPayload != null)
+            {
+                //Do DateTime conversion from u type back into DateTime object
+                DateTime dateCreated;
+                DateTime.TryParseExact(jsonPayload["date_created"].ToString(), "u", null, DateTimeStyles.AdjustToUniversal, out dateCreated);
+
+                //Get the details of the user from the JWT payload
+                userAuth.UserId = Convert.ToInt32(jsonPayload["user_id"]);
+                userAuth.UserName = jsonPayload["username"].ToString();
+                userAuth.UserType = jsonPayload["user_type"].ToString();
+                userAuth.DateCreated = dateCreated;
+                userAuth.AuthToken = jwtToken;
+            }
+
+            //Return the object
+            return userAuth;
+        }
+
+    }
+}
---
+++ Umbraco.AuthTokens/Data/UmbracoAuthTokenSecret.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Umbraco.AuthTokens.Data
+{
+    public class UmbracoAuthTokenSecret
+    {
+        private const string SecretEnvVariable = "Umbraco.AuthToken";
+
+        /// <summary>
+        /// This sets the secret as an Environment Variable
+        /// </summary>
+        /// <param name="secret">Secret string to set</param>
+        public void SetSecret(string secret)
+        {
+            Environment.SetEnvironmentVariable(SecretEnvVariable, secret, EnvironmentVariableTarget.Machine);
+        }
+
+        /// <summary>
+        /// Goes & fetchs the secret from the Machine Environment Variables
+        /// </summary>
+        /// <returns>Returns the string secret</returns>
+        public string GetSecret()
+        {
+            return Environment.GetEnvironmentVariable(SecretEnvVariable, EnvironmentVariableTarget.Machine);
+        }
+    }
+}
