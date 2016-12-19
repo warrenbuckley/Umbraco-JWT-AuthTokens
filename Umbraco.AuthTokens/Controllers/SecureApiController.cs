d7e43f94dcc6cc7830c25edcb4da0c37495b1a58
oromand <oromand>
2016-12-19 21:49:56 +0100

    Added source of version 1.1.0.0. Credits go to https://github.com/warrenbuckley/Umbraco-JWT-AuthTokens.

--- Umbraco.AuthTokens/Controllers/SecureApiController.cs
+++ Umbraco.AuthTokens/Controllers/SecureApiController.cs
@@ -1,61 +1,116 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Security;
+using Umbraco.Core.Configuration;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 using UmbracoAuthTokens.Data;
-
+ 
 namespace UmbracoAuthTokens.Controllers
 {
     [PluginController("TokenAuth")]
     public class SecureApiController : UmbracoApiController
     {
         /// <summary>
-        /// http://localhost:49683/umbraco/TokenAuth/SecureApi/Authorise
+        /// http://localhost:55778/umbraco/TokenAuth/SecureApi/AuthoriseUser
         /// </summary>
         /// <returns>A JWT token as a string if auth is valid</returns>
         [HttpPost]
-        public string Authorise(AuthCredentials auth)
+        public string AuthoriseUser(AuthCredentials auth)
         {
             //Verify user is valid credentials
             var isValidAuth = Security.ValidateBackOfficeCredentials(auth.Username, auth.Password);
-
+ 
             //Are credentials correct?
             if (isValidAuth)
             {
                 //Get the backoffice user from username
                 var user = ApplicationContext.Services.UserService.GetByUsername(auth.Username);
-
+ 
                 //Check if we have an Auth Token for user
                 var hasAuthToken = UserAuthTokenDbHelper.GetAuthToken(user.Id);
-
+ 
                 //If the token already exists
                 if (hasAuthToken != null)
                 {
                     //Lets just return it in the request
                     return hasAuthToken.AuthToken;
                 }
-
+ 
                 //Else user has no token yet - so let's create one
                 //Generate AuthToken DB object
                 var newToken = new UmbracoAuthToken();
                 newToken.IdentityId = user.Id;
                 newToken.IdentityType = IdentityAuthType.User.ToString();
-
+ 
                 //Generate a new token for the user
-                var authToken = UmbracoAuthTokenFactory.GenerateUserAuthToken(newToken);
-
+                var authToken = UmbracoAuthTokenFactory.GenerateAuthToken(newToken);
+ 
                 //We insert authToken as opposed to newToken
                 //As authToken now has DateTime & JWT token string on it now
-
+ 
                 //Store in DB (inserts or updates existing)
                 UserAuthTokenDbHelper.InsertAuthToken(authToken);
-
+ 
                 //Return the JWT token as the response
                 //This means valid login & client in our case mobile app stores token in local storage
                 return authToken.AuthToken;
             }
-
+ 
+            //Throw unauthorised HTTP error
+            var httpUnauthorised = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            throw new HttpResponseException(httpUnauthorised);
+        }
+ 
+        /// <summary>
+        /// http://localhost:55778/umbraco/TokenAuth/SecureApi/AuthoriseMember
+        /// </summary>
+        /// <returns>A JWT token as a string if auth is valid</returns>
+        [HttpPost]
+        public string AuthoriseMember(AuthCredentials auth)
+        {
+             
+            //Verify user is valid credentials - using current membership provider
+            //Should be native Umbraco one
+            var isValidAuth = Membership.ValidateUser(auth.Username, auth.Password);
+ 
+            //Are credentials correct?
+            if (isValidAuth)
+            {
+                //Get the member from username
+                var member = ApplicationContext.Services.MemberService.GetByUsername(auth.Username);
+ 
+                //Check if we have an Auth Token for user
+                var hasAuthToken = UserAuthTokenDbHelper.GetAuthToken(member.Id);
+ 
+                //If the token already exists
+                if (hasAuthToken != null)
+                {
+                    //Lets just return it in the request
+                    return hasAuthToken.AuthToken;
+                }
+ 
+                //Else user has no token yet - so let's create one
+                //Generate AuthToken DB object
+                var newToken = new UmbracoAuthToken();
+                newToken.IdentityId = member.Id;
+                newToken.IdentityType = IdentityAuthType.Member.ToString();
+ 
+                //Generate a new token for the user
+                var authToken = UmbracoAuthTokenFactory.GenerateAuthToken(newToken);
+ 
+                //We insert authToken as opposed to newToken
+                //As authToken now has DateTime & JWT token string on it now
+ 
+                //Store in DB (inserts or updates existing)
+                UserAuthTokenDbHelper.InsertAuthToken(authToken);
+ 
+                //Return the JWT token as the response
+                //This means valid login & client in our case mobile app stores token in local storage
+                return authToken.AuthToken;
+            }
+ 
             //Throw unauthorised HTTP error
             var httpUnauthorised = new HttpResponseMessage(HttpStatusCode.Unauthorized);
             throw new HttpResponseException(httpUnauthorised);
--- Umbraco.AuthTokens/Data/UmbracoAuthTokenSecret.cs
+++ Umbraco.AuthTokens/Data/UmbracoAuthTokenSecret.cs
@@ -1,42 +1,39 @@
 ﻿using System;
 using System.Web.Security;
-
+ 
 namespace UmbracoAuthTokens.Data
 {
     public static class UmbracoAuthTokenSecret
     {
         private const string SecretEnvVariable = "Umbraco.AuthToken";
-
-        //TODO: Check EnvVariable works well with Azure Websites (No access to server itself)
-        //Same with Umbraco.io/Umbraco.com perhaps? If they have a UI to set, add & edit them?
-
+         
         /// <summary>
         /// This sets the secret as an Environment Variable
         /// </summary>
         /// <param name="secret">Secret string to set</param>
         public static void SetSecret(string secret)
         {
-            Environment.SetEnvironmentVariable(SecretEnvVariable, secret,EnvironmentVariableTarget.Machine);
+            Environment.SetEnvironmentVariable(SecretEnvVariable, secret);
         }
-
+ 
         /// <summary>
         /// Goes & fetchs the secret from the Machine Environment Variables
         /// </summary>
         /// <returns>Returns the string secret</returns>
         public static string GetSecret()
         {
-            var secret = Environment.GetEnvironmentVariable(SecretEnvVariable, EnvironmentVariableTarget.Machine);
-
+            var secret = Environment.GetEnvironmentVariable(SecretEnvVariable);
+ 
             //If it does not exist or is null/empty then we set a new one
             if (string.IsNullOrEmpty(secret))
             {
                 //Lets create a random strong password & set env variable
                 secret =  Membership.GeneratePassword(50, 5);
-
+ 
                 //Set it as the Env Var
                 SetSecret(secret);
             }
-
+ 
             //Return the secret
             return secret;
         }
