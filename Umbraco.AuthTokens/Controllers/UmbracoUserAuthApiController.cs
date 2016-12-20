2e1187b5e33946171b3b0d19aa7b8e1423026254
Olivier ROMAND <oliv.rm@gmail.com>
2016-12-20 09:40:14 +0100

    Fixed wrong references

--- Umbraco.AuthTokens/Controllers/UmbracoUserAuthApiController.cs
+++ Umbraco.AuthTokens/Controllers/UmbracoUserAuthApiController.cs
@@ -1,21 +1,21 @@
-﻿using Umbraco.Core.Models;
+﻿using Umbraco.Core.Models.Membership;
 using Umbraco.Web.WebApi;
- 
+
 namespace UmbracoAuthTokens.Controllers
 {
-    public abstract class UmbracoMemberAuthApiController : UmbracoApiController
+    public abstract class UmbracoUserAuthApiController : UmbracoApiController
     {
         /// <summary>
         /// When a user has been authorised with a JWT Auth token
-        /// This is the member represents the authorised member
+        /// This is the backofficer user that represents the authorised user
         /// that can then be used in the API controller for Umbraco API Service Calls 
         /// such as creating content
         /// </summary>
-        public IMember AuthorisedMember
+        public IUser AuthorisedBackofficeUser
         {
             get
             {
-                return ControllerContext.Request.Properties["umbraco-member"] as IMember;
+                return ControllerContext.Request.Properties["umbraco-user"] as IUser;
             }
         }
     }
